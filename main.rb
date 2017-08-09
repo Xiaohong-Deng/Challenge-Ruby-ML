208e96dacf70c2a705bd23f248bb5bebe30a066e
nicenick <worldsky86rough@gmail.com>
2017-08-10 04:09:19 +0800

    Add full logic of back and forward prop

--- main.rb
+++ main.rb
@@ -4,18 +4,18 @@
 require 'set'
 load 'neuro.rb'
 
-# We will use the iris data set based which contains observations of 
+# We will use the iris data set based which contains observations of
 # flower petal and sepal size to predict the flower genus
 # Dataset: http://en.wikipedia.org/wiki/Iris_flower_data_set
 
 data = File.readlines("iris.csv").map {|l| l.chomp.split(',') }
 
 class_label = {
-  "s" => [1, 0, 0], 
-  "c" => [0, 1, 0], 
+  "s" => [1, 0, 0],
+  "c" => [0, 1, 0],
   "v" => [0, 0 ,1]
 }
-
+# byebug
 x = data.map {|x| x[0,4].map(&:to_f) }
 y = data.map {|x| class_label[x[4]] }
 
@@ -33,9 +33,9 @@ x.map! do |row|
     end
 end
 
-# We split the iris data so that a each class has the same 
+# We split the iris data so that a each class has the same
 # probability to appear in each sub set (train and test) as
-# it has in the full set #stratified sampling.For the iris 
+# it has in the full set #stratified sampling.For the iris
 # data set the propabilities are equal.
 
 # number of elements per class in the training set.
@@ -44,11 +44,11 @@ n_train = 25
 d1 = (0..49).to_a.sample(n_train)
 d2 = (50..99).to_a.sample(n_train)
 d3 = (100..149).to_a.sample(n_train)
-
+# byebug
 # samples we want in the train set.
 train_samples = (d1 + d2 + d3).to_set
 
-# samples we want in the test set are 
+# samples we want in the test set are
 # those not in the training set.
 test_samples = (0..149).to_set - train_samples
 
@@ -57,20 +57,20 @@ y_train = Matrix.rows(y.values_at(*train_samples))
 
 x_test = Matrix.rows(x.values_at(*test_samples))
 y_test = Matrix.rows(y.values_at(*test_samples))
-
+byebug
 # creates a new neural network
 nn = NeuralNetwork.new()
 
 # trains the network with the training set
-# and predicts the classes of the unseen 
+# and predicts the classes of the unseen
 # test data
 nn.train(x_train, y_train)
 y_hat = nn.predict(x_test)
 
 # prints the networks "accuracy" in predicting
-# the unseen test set. 
+# the unseen test set.
 cm = confusion_matrix(y_test, y_hat)
-p "Confusion Matrix:" 
+p "Confusion Matrix:"
 p cm.to_s
 p "Accuracy: " + accuracy(cm).to_s
 
--- neuro.rb
+++ neuro.rb
@@ -1,6 +1,7 @@
 require 'matrix'
+require "byebug"
 
-# Matrix element wise matrix multiplication 
+# Matrix element wise matrix multiplication
 # Hadamard product (matrices):
 # https://en.wikipedia.org/wiki/Hadamard_product_(matrices)
 def element_multiplication(m1, m2)
@@ -21,11 +22,12 @@ end
 def confusion_matrix(expected, predicted)
 	expected = expected.to_a.map {|x| x.index(x.max)}
 	predicted = predicted.to_a.map {|x| x.index(x.max)}
-	
+
 	n = (expected + predicted).uniq.length
 	cm = Matrix.build(n){0}.to_a
+	# byebug
 	expected.zip(predicted).map {|x, y| cm[x][y]+=1}
-	
+
 	return Matrix.rows(cm)
 end
 
@@ -37,12 +39,18 @@ class NeuralNetwork
 		# network. One hidden layer, one input layer, and one
 		# output layer are enough to achieve 99% accuracy on
 		# the data set.
+		@eta = 0.1
+		@hidden_nodes = 3
+		@iteration = 500
+		# initial weights are in the range of [-init_w_range, +init_w_range]
+		@init_w_range = 0.1
 	end
 
 	##############################################
 	def train(x, y)
 		# the training method that updates the internal weights
 		# using the predict
+		@weights_ij, @weights_ij = back_and_forward_prop(x, y)
 	end
 
 	##############################################
@@ -51,9 +59,27 @@ class NeuralNetwork
 
 	##############################################
 	protected
+		def back_and_forward_prop(x, y)
+			x0 = Array.new(x.row_size, 1)
+			x = matrix.columns(x.transpose.to_a.unshift(x0))
+			weights_ij = init_weights(x.column_size, @hidden_nodes)
+			weights_jk = init_weights(@hidden_nodes+1, 1)
+			@iteration.times do
+				row_to_pick = rand(x.row_size+1)
+				x_rand = x.row(row_to_pick)
+				y_rand = y.row(row_to_pick)
+				x_hidden, output, s_output, s_hidden = propagate(x, weights_ij, weights_jk)
+				delta_hidden, delta_output = back_propagate(x_rand, y_rand, x_hidden, output, weight_jk, S_output, S_hidden)
+				grad_matrix_ij = @eta * (delta_hidden * x_rand).transpose
+				weights_ij -= grad_matrix_ij
+				grad_matrix_jk = @eta * (delta_output * x_hidden)
+				weights_jk -= grad_matrix_jk.transpose
+			end
+			[weights_ij, weights_jk]
+		end
 
 		##############################################
-		def propagate(x)
+		def propagate(x, weights_ij, weights_jk)
 			# applies the input to the network
 			# this is the forward propagation step
 		end
@@ -62,5 +88,11 @@ class NeuralNetwork
 		def back_propagate(x, y, y_hat)
 			# goes backwards and finds the weights
 			# that need to be tuned
+		end
+
+	private
+		def init_weights(rows, cols)
+			rand_num = 2 * @init_w_range * rand - @init_w_range
+			Matrix.rows(Array.new(rows) { Array.new(cols) { rand_num } })
 		end
 end
